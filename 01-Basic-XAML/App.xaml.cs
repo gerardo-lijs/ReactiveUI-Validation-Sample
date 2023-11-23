0b4cc2645ffa92bd5d30582c9386ecc102435e91
Gerardo Lijs <gerardo@micropticsl.com>
2023-11-23 19:16:53 +0100

    apply editor config code style

--- 01-Basic-XAML/App.xaml.cs
+++ 01-Basic-XAML/App.xaml.cs
@@ -1,14 +1,13 @@
-﻿using System.Windows;
+﻿namespace ReactiveUIValidationSample;
 
-namespace ReactiveUIValidationSample
+using System.Windows;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    // NB: For ReactiveUI Validation to work properly we can't use StartupUri
+    private void Application_Startup(object sender, StartupEventArgs e)
     {
-        // NB: For ReactiveUI Validation to work properly we can't use StartupUri
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
-            var mainWindow = new MainWindow();
-            mainWindow.Show();
-        }
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
     }
 }
--- 01-Basic-XAML/MainWindow.xaml.cs
+++ 01-Basic-XAML/MainWindow.xaml.cs
@@ -1,40 +1,39 @@
-﻿using ReactiveUI;
+﻿namespace ReactiveUIValidationSample;
+
+using ReactiveUI;
 using ReactiveUI.Validation.Extensions;
 using System.Reactive.Disposables;
 
-namespace ReactiveUIValidationSample
+public partial class MainWindow : ReactiveWindow<MainViewModel>
 {
-    public partial class MainWindow : ReactiveWindow<MainViewModel>
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        ViewModel = new MainViewModel();
+        this.WhenActivated(disposableRegistration =>
         {
-            InitializeComponent();
-            ViewModel = new MainViewModel();
-            this.WhenActivated(disposableRegistration =>
-            {
-                this.WhenAnyValue(view => view.ViewModel).BindTo(this, view => view.DataContext).DisposeWith(disposableRegistration);
+            this.WhenAnyValue(view => view.ViewModel).BindTo(this, view => view.DataContext).DisposeWith(disposableRegistration);
 
-                this.Bind(ViewModel, viewModel => viewModel.SaveResult, view => view.SaveResultTextBlock.Text).DisposeWith(disposableRegistration);
-                this.BindCommand(ViewModel, viewModel => viewModel.Save, view => view.SaveButton).DisposeWith(disposableRegistration);
+            this.Bind(ViewModel, viewModel => viewModel.SaveResult, view => view.SaveResultTextBlock.Text).DisposeWith(disposableRegistration);
+            this.BindCommand(ViewModel, viewModel => viewModel.Save, view => view.SaveButton).DisposeWith(disposableRegistration);
 
-                // 2019-10-31 - For Validation to work in WPF with ReactiveUI, we can't use code behind bindings. We need to bind in XAML.
-                // https://reactivex.slack.com/archives/C02AJB872/p1571739816086600?thread_ts=1571711238.085100
-                //this.Bind(ViewModel, viewModel => viewModel.FirstName, view => view.FirstNameTextBox.Text).DisposeWith(disposableRegistration);
-                //this.Bind(ViewModel, viewModel => viewModel.LastName, view => view.LastNameTextBox.Text).DisposeWith(disposableRegistration);
+            // 2019-10-31 - For Validation to work in WPF with ReactiveUI, we can't use code behind bindings. We need to bind in XAML.
+            // https://reactivex.slack.com/archives/C02AJB872/p1571739816086600?thread_ts=1571711238.085100
+            //this.Bind(ViewModel, viewModel => viewModel.FirstName, view => view.FirstNameTextBox.Text).DisposeWith(disposableRegistration);
+            //this.Bind(ViewModel, viewModel => viewModel.LastName, view => view.LastNameTextBox.Text).DisposeWith(disposableRegistration);
 
-                // Validation extension methods. They are not working as expected or I'm using them in the wrong way...
-                // Binding to a TextBlock text I get errors concatenated. Tried with ListView, etc with no luck.
-                this.BindValidation(ViewModel, view => view.ErrorsTextBlock.Text).DisposeWith(disposableRegistration);
+            // Validation extension methods. They are not working as expected or I'm using them in the wrong way...
+            // Binding to a TextBlock text I get errors concatenated. Tried with ListView, etc with no luck.
+            this.BindValidation(ViewModel, view => view.ErrorsTextBlock.Text).DisposeWith(disposableRegistration);
 
-                // Validation extension methods. They are not working as expected or I'm using them in the wrong way...
-                // Getting System.ArgumentOutOfRangeException: 'Index was out of range. Must be non-negative and less than the size of the collection. Parameter name: index'
-                // in the second call to BindValidation on a different Property
-                //this.BindValidation(ViewModel, viewModel => viewModel.FirstName, view => view.FirstNameTextBox.ToolTip).DisposeWith(disposableRegistration);
-                //this.BindValidation(ViewModel, viewModel => viewModel.LastName, view => view.LastNameTextBox.ToolTip).DisposeWith(disposableRegistration);
+            // Validation extension methods. They are not working as expected or I'm using them in the wrong way...
+            // Getting System.ArgumentOutOfRangeException: 'Index was out of range. Must be non-negative and less than the size of the collection. Parameter name: index'
+            // in the second call to BindValidation on a different Property
+            //this.BindValidation(ViewModel, viewModel => viewModel.FirstName, view => view.FirstNameTextBox.ToolTip).DisposeWith(disposableRegistration);
+            //this.BindValidation(ViewModel, viewModel => viewModel.LastName, view => view.LastNameTextBox.ToolTip).DisposeWith(disposableRegistration);
 
-                this.BindValidationToToolTip(ViewModel, viewModel => viewModel.FirstName, view => view.FirstNameTextBox).DisposeWith(disposableRegistration);
-                this.BindValidationToToolTip(ViewModel, viewModel => viewModel.LastName, view => view.LastNameTextBox).DisposeWith(disposableRegistration);
-            });
-        }
+            this.BindValidationToToolTip(ViewModel, viewModel => viewModel.FirstName, view => view.FirstNameTextBox).DisposeWith(disposableRegistration);
+            this.BindValidationToToolTip(ViewModel, viewModel => viewModel.LastName, view => view.LastNameTextBox).DisposeWith(disposableRegistration);
+        });
     }
 }
